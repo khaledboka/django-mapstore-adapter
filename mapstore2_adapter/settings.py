2cfb9c3ec3fc2512c460436ad2f3536170344c7f
afabiani <alessio.fabiani@geo-solutions.it>
2018-07-12 18:08:57 +0200

    - Enable MAP_DEBUG Context Variable

--- mapstore2_adapter/settings.py
+++ mapstore2_adapter/settings.py
@@ -12,6 +12,12 @@
 from django.conf import settings
 
 
+MAP_DEBUG = getattr(settings, "MAPSTORE_DEBUG", False)
 MAP_BASELAYERS = getattr(settings, "MAPSTORE_BASELAYERS", [])
 CATALOGUE_SERVICES = getattr(settings, "MAPSTORE_CATALOGUE_SERVICES", {})
 CATALOGUE_SELECTED_SERVICE = getattr(settings, "MAPSTORE_CATALOGUE_SELECTED_SERVICE", None)
+
+try:
+    settings.TEMPLATES[0]['OPTIONS']['context_processors'] += ['mapstore2_adapter.context_processors.resource_urls',]
+except:
+    pass
