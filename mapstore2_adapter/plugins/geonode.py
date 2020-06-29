5610f062dd6c0031deddf01ed37f65908e9ee8f6
afabiani <alessio.fabiani@geo-solutions.it>
2020-06-29 14:54:12 +0200

    - Bump to version 2.0.4.3

--- mapstore2_adapter/plugins/geonode.py
+++ mapstore2_adapter/plugins/geonode.py
@@ -555,8 +555,8 @@ class GeoNodeMapStore2ConfigConverter(BaseMapStore2ConfigConverter):
             output: GeoNode JSON Gxp Config
         """
         # MapStore uses x0,y0,x1,y1 ordering of bbox coords
-        if map and map.get('bbox'):
-            ms2_bbox = map.get('bbox')
+        if viewer.get('map', None) and viewer['map'].get('bbox'):
+            ms2_bbox = viewer['map'].get('bbox')
             config_bbox = [ms2_bbox[0], ms2_bbox[2], ms2_bbox[1], ms2_bbox[3]]
             viewer['map']['bbox'] = config_bbox
         return to_json(viewer)
