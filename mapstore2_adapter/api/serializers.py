205ed97b0c9642a515e86d6b5c9d1dedc29a574a
afabiani <alessio.fabiani@geo-solutions.it>
2020-12-01 18:06:02 +0100

    - Restore "MapLayersJSONArraySerializerField"
    (cherry picked from commit a1e6dd6a804d01ba27f05fc867b304fa2cbbde51)

--- mapstore2_adapter/api/serializers.py
+++ mapstore2_adapter/api/serializers.py
@@ -62,6 +62,19 @@ class JSONArraySerializerField(serializers.Field):
         return attributes
 
 
+class MapLayersJSONArraySerializerField(serializers.Field):
+
+    def to_internal_value(self, data):
+        return data
+
+    def to_representation(self, value):
+        if value:
+            from geonode.maps.models import Map
+            from geonode.maps.api.serializers import MapLayerSerializer
+            map = Map.objects.get(id=value)
+            return MapLayerSerializer(embed=True, many=True).to_representation(map.layers)
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
@@ -72,6 +85,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class MapStoreResourceSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.CharField(source='user.username',
                                  read_only=True)
+    layers = MapLayersJSONArraySerializerField(source='id',
+                                               read_only=True)
 
     def __init__(self, *args, **kwargs):
         # Instantiate the superclass normally
@@ -84,4 +99,4 @@ class MapStoreResourceSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = MapStoreResource
-        fields = ('id', 'user', 'name', 'creation_date', 'last_update')
+        fields = ('id', 'user', 'layers', 'name', 'creation_date', 'last_update')
--- mapstore2_adapter/api/views.py
+++ mapstore2_adapter/api/views.py
@@ -15,6 +15,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly  # noqa
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication
+from geonode.base.api.permissions import IsOwnerOrReadOnly
 
 from .models import MapStoreResource
 from .serializers import (UserSerializer,
@@ -40,7 +41,7 @@ class MapStoreResourceViewSet(viewsets.ModelViewSet):
     """ Only Authenticate User perform CRUD Operations on Respective Data
     """
     authentication_classes = [SessionAuthentication, BasicAuthentication, OAuth2Authentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     model = MapStoreResource
     serializer_class = MapStoreResourceSerializer
 
